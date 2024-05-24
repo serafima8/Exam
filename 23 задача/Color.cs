1ef1d06f264a943ddd7f975ca2258fcc62081604
serafima8 <147659413+serafima8@users.noreply.github.com>
2024-05-24 14:20:26 +0300

    Add files via upload

---
+++ 23 задача/Color.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_задача
+{
+    static class Color
+    {
+        public enum Colors
+        {
+            Black,
+            Blue,
+            Green,
+            Yellow,
+            Red,
+            Magenta,
+            Cyan,
+            White
+        }
+        static public void Print (string stroka, int color)
+        {
+            switch((Colors)color)
+            {
+                case Colors.Black:
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case Colors.Blue:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case Colors.Green:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case Colors.Yellow:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case Colors.Red:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case Colors.Magenta:
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    break;
+                case Colors.Cyan:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    break;
+                case Colors.White:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+            Console.WriteLine(stroka);
+        }
+    }
+}
---
+++ 23 задача/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_задача
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string stroka = Console.ReadLine();
+            Console.WriteLine("Каким цветом вывести текст? ");
+            foreach(Color.Colors c in Enum.GetValues(typeof (Color.Colors)))
+            {
+                Console.WriteLine($"{(int)c} - {c}");
+            }
+            Console.WriteLine("Введите цифру цвета: ");
+            int color = Convert.ToInt32(Console.ReadLine());
+            Color.Print(stroka, color);
+
+            Console.ReadLine();
+        }
+    }
+}
