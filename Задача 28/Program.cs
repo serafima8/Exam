df24423ddb504c2f47fdd4bfcb427d11d6cb480d
serafima8 <147659413+serafima8@users.noreply.github.com>
2024-05-31 13:45:55 +0300

    Add files via upload

---
+++ Задача 28/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tour tour = new Tour();
+            tour.TourCalc();
+            tour.TourCalc("Греция");
+            tour.TourCalc("Италия", 10);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача 28/Tour.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_28
+{
+    class Tour
+    {
+        private string name;
+        private double price;
+        private double kolvoDays;
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double KolvoDays { get; set; }
+
+        public Tour() { }
+        public Tour(string name, double price, double kolvoDays)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.KolvoDays = kolvoDays;
+        }
+        public void TourCalc()
+        {
+            Console.WriteLine("Минское море, бесплатно");
+        }
+        public void TourCalc(string name)
+        {
+            kolvoDays = 1;
+            price = 50;
+            Console.WriteLine($"{name} - {kolvoDays} день {price} руб.");
+        }
+        public void TourCalc(string name,double kolvoDays)
+        {
+            Console.WriteLine($"{name}\tКоличество дней: {kolvoDays}\tСтоимость: {50 * kolvoDays}");
+        }
+    }
+}
